dcbd2750bde554df0b3bbb7dbe22b437e5165552
chasingOblivion <simon.ts.yu@gmail.com>
2020-12-05 17:57:14 -0800

    identify which square user has clicked

--- script.js
+++ script.js
@@ -28,11 +28,25 @@ const display = () => {
     Object.keys(gameBoard.getBoard()).forEach(function(key) {
       const square = document.createElement('div');
       square.classList.add('square');
+      square.setAttribute('data-index', key);
       const text = document.createElement('div');
       text.textContent = key;
       square.appendChild(text);
       board.appendChild(square);
     });
+
+    // add event listeners to squares
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+      square.addEventListener('click', () => {
+        userPlay(square);
+      });
+    });
+}
+
+const userPlay = (square) => {
+  const clicked = square.dataset.index;
+  console.log("you clicked: " + clicked);
 }
 
 display();
